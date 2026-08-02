feb6bfe31882c4d2137762aa8d76774e450c7ca4
Jay Mingle <jkawudi@gmail.com>
2026-08-02 01:30:12 +0000

    Add responsive Title component for iOS and Android

---
+++ components/ui/Title.android.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, Dimensions, Platform} from "react-native";
+
+function Title({children}) {
+    return <Text style={styles.title}>{children}</Text>
+}
+
+export default Title;
+
+const deviceWidth = Dimensions.get('window').width
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: deviceWidth < 380 ? 18 : 24,
+        color: 'white',
+        textAlign: 'center',
+        // borderWidth: Platform.OS === 'android' ? 2 : 0,
+        // borderWidth: Platform.select({ios: 0, android: 5}),
+        borderWidth: 2,
+        borderColor: 'white',
+        padding: 12,
+        fontFamily: 'open-sans-bold',
+        maxWidth:  '80%',
+        width: 300
+    }
+})
